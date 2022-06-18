5b87dd85fd6b5055e7fb03155c5bb5e04989afb7
devdan <oluochdancun19@gmail.com>
2022-06-18 16:13:33 +0300

    commit tree 2b3ea7f34bc37562f632438ee9b633dbe40d15dc

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+
+function submitData(name, email){
+    return fetch("http://localhost:3000/users",  {
+        method:"POST",
+        headers:{
+            "content-Type":"application/json",
+            "Accept":"application/json"
+        },
+        body:JSON.stringify({
+            name, email
+        })
+    })
+    .then((res)=>{
+        return res.json();
+    })
+    .then((user)=>{
+        document.body.innerHTML = user["id"]
+    })
+    .catch((error)=>{
+        return error;
+    })
+}
